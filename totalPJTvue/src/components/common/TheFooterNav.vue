0c8768caf1ec275a8080b390e4ce104b7c36c25a
Yejin Song <yaejin5195@naver.com>
2024-05-13 18:00:05 +0900

    fix : nav-bar

--- totalPJTvue/src/components/common/TheFooterNav.vue
+++ totalPJTvue/src/components/common/TheFooterNav.vue
@@ -5,6 +5,7 @@
         <div>
           <RouterLink :to="{ name: 'search' }"
             ><i class="bi bi-search" style="font-size: 50px; color: white"></i>
+            <p>검색</p>
           </RouterLink>
         </div>
         <div>
@@ -12,32 +13,36 @@
             ><i
               class="bi bi-bookmark-heart"
               style="font-size: 50px; color: white"
-            ></i
-          ></RouterLink>
+            ></i>
+            <p>즐겨찾기</p>
+          </RouterLink>
         </div>
         <div>
           <RouterLink to="/"
             ><i
               class="bi bi-house-fill"
               style="font-size: 80px; color: white"
-            ></i
-          ></RouterLink>
+            ></i>
+            <p>홈</p>
+          </RouterLink>
         </div>
         <div>
           <RouterLink :to="{ name: 'calendar' }"
             ><i
               class="bi bi-calendar-check"
               style="font-size: 50px; color: white"
-            ></i
-          ></RouterLink>
+            ></i>
+            <p>일정</p>
+          </RouterLink>
         </div>
         <div>
           <RouterLink :to="{ name: 'myPage' }"
             ><i
               class="bi bi-person-fill"
               style="font-size: 50px; color: white"
-            ></i
-          ></RouterLink>
+            ></i>
+            <p>마이페이지</p>
+          </RouterLink>
         </div>
       </nav>
     </footer>
@@ -47,8 +52,14 @@
 <script setup></script>
 
 <style scoped>
-/* footer nav {
-  margin: 0;
-  padding: 0;
-} */
+footer nav div a.router-link-exact-active i::before {
+  color: gray;
+}
+a p {
+  text-align: center;
+  text-decoration: none;
+}
+a i::before {
+  align-items: center;
+}
 </style>
